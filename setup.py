97a79b82912a94699d94d0ffec1ab9d2eeed242a
Stefan Bertici <stefan.bertici@travelport.com>
2025-02-13 11:28:44 +0200

    added setup and pyproject

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chatwrap',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add your project dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            # Add command line scripts here
+        ],
+    },
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A brief description of your project',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/chatwrap',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
